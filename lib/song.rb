8df9dd54073f5618c0efc54acb4f5056fbd7b9ea
Rebecca Strauss <rstrauss127@gmail.com>
2018-02-07 19:47:53 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,30 +1,30 @@
 class Song
-attr_accessor :name, :artist, :genre 
-@@count = 0 
-@@genre = []
-@@artist = []
-
-def initialize(name, artist, genre)
-  @name = name
-  @artist = artist
-  @genre = genre
+  attr_accessor :name, :artist, :genre 
+  @@count = 0 
+  @@genre = []
+  @@artists = []
   
-  @@count += 1
+  def initialize(name, artist, genre)
+    @name = name
+    @artist = artist
+    @genre = genre
+    
+    @@count += 1
+    
+    @@genre << genre unless @@genre.include?(genre)
+    @@artist << artist 
+  end
   
-  @@genre << genre unless @@genre.include?(genre)
-  @@artist << artist unless @@artist.include?(artist)
-end
-
-def self.count
-  @@count
-end
-
-def self.genres 
-  @@genre 
-end
-
-def self.artists
-  @@artist
-end
+  def self.count
+    @@count
+  end
+  
+  def self.genres 
+    @@genre 
+  end
+  
+  def self.artists
+    @@artist
+  end
 
 end
